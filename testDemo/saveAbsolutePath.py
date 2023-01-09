e2a085eb574b3228bb569d3a9dc7d8086e05f1c1
Yang-cola <1549029765@qq.com>
2023-01-09 13:55:10 +0800

    整理路径

---
+++ testDemo/saveAbsolutePath.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+""" 
+@Time:        2023/1/9 11:21
+@Author:      CookieYang
+@FileName:    saveAbsolutePath.py
+@SoftWare:    PyCharm
+@brief:       绝对路径存储
+"""
+
+import os
+
+if __name__ == '__main__':
+    path = "name.txt"
+    open(path, 'w')
+
+
+
+
+
+
+
+
+
+
+    # res = os.path.join("../class_study", path)
+    # print(res)
+    # open(res, 'w')
+
