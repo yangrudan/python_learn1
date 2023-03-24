7ef376d86a2da13da597e5901cad06b1cd08f10d
Yang-cola <1549029765@qq.com>
2023-03-24 09:49:54 +0800

    填充表

--- dbL/demo_mysql.py
+++ dbL/demo_mysql.py
@@ -12,14 +12,32 @@ mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   passwd="13141996jyD@",
-  auth_plugin='mysql_native_password'
+  auth_plugin='mysql_native_password',
+  database="mydatabase"
 )
 
 mycursor = mydb.cursor()
 
-# mycursor.execute("CREATE DATABASE mydatabase")
+# mycursor.execute("CREATE DATABASE mydatabase")  # 创建数据库
 
-mycursor.execute("SHOW DATABASES")
+# mycursor.execute("SHOW DATABASES")  # 检查数据库
 
-for x in mycursor:
-  print(x)
+# mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")  # 创建表
+
+# mycursor.execute("SHOW TABLES")  # 返回数据库列表
+
+# Table 'customers' already exists 创建表时创建主键
+# mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
+
+# mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+
+"""
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = ("John", "Highway 21")
+mycursor.execute(sql, val)
+
+mydb.commit()
+
+print(mycursor.rowcount, "record inserted.")
+
+"""
