7aa9667a806394d16323416c258ac00afe9d8f4d
Yang-cola <1549029765@qq.com>
2023-03-24 10:12:34 +0800

    数据库的创建、插入、更新、筛选、删除、组合

--- dbL/demo_mysql.py
+++ dbL/demo_mysql.py
@@ -88,3 +88,28 @@ myresult = mycursor.fetchone()
 
 print(myresult)
 """
+
+# %
+"""
+sql = "SELECT * FROM customers WHERE address LIKE '%way%'"
+
+mycursor.execute(sql)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+"""
+
+# 转义查询值
+"""
+sql = "SELECT * FROM customers WHERE address = %s"
+adr = ("Yellow Garden 2", )
+
+mycursor.execute(sql, adr)
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
+"""
