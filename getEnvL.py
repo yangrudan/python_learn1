f70d314752e1ba5f02315c06b2100ea7b182ec24
Yang-cola <1549029765@qq.com>
2023-04-11 17:28:57 +0800

    dotenv的环境变量配置

---
+++ getEnvL.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+""" 
+@Time:        2023/4/11 17:06
+@Author:      CookieYang
+@FileName:    getEnvL.py
+@SoftWare:    PyCharm
+@brief:       功能简介
+"""
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+key = 'MY_NAME'
+
+value = os.getenv(key)
+
+print("Value of 'HOME' environment variable :", value)
