777f37631accf4af6bb2c11c15d647f6cfe19921
Cookie <1549029765@qq.com>
2023-10-08 11:05:35 +0800

    多个阵

--- jsonL/dict2json.py
+++ jsonL/dict2json.py
@@ -6,6 +6,7 @@ import numpy as np
 result = np.linspace(0, 512*1.2, 512).tolist()  # 精度不好看
 
 data = {
+    "array name": "阵1",
     "sampling rate": 5000,
     "detection azimuth" : [0,180],
     "operational band": [10,625],
@@ -15,5 +16,8 @@ data = {
 
 }
 
-json_str = json.dumps(data)
+
+out_list = [data] * 2
+
+json_str = json.dumps(out_list)
 pprint(json_str)
