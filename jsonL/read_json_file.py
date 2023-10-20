2a3949e0d30eb0601086a89b606040bd954201b8
Cookie <1549029765@qq.com>
2023-10-20 19:46:21 +0800

    read json file

--- jsonL/read_json_file.py
+++ jsonL/read_json_file.py
@@ -1,9 +1,11 @@
 import json
+import os
 
-def search_file_for_array(array_name="array-1"):
-    read_fp = open("myfile.json","r")
-    data = json.load(read_fp)
-    print(type(data))
+def search_file_for_array(json_file_path="myfile.json", array_name="array-1"):
+    if os.path.exists(json_file_path):
+        read_fp = open(json_file_path,"r")
+        data = json.load(read_fp)
+        print(type(data))
 
     for element in data:
         if element["array_name"] == array_name:
