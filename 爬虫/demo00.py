eb34378b7579cfa9b755c481c8f0161244e999bb
Cookie <1549029765@qq.com>
2024-02-27 10:42:19 +0800

    how login?

--- 爬虫/demo00.py
+++ 爬虫/demo00.py
@@ -3,8 +3,10 @@ Copyright (c) Cookie Yang. All right reserved.
 """
 import requests
 
-
-response = requests.get("http://books.toscrape.com/")
+data = {'username': '002438',
+        'password': "13141996jyD"}
+headers = {'user-agent': 'my-app/0.0.1'}
+response = requests.post("https://portal.zhejianglab.com/oa/home/login", data=data, headers=headers)
 if response.ok:
     print(response.text)
 else:
