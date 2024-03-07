5b4a8691c90ac659bc70d742c0fb60ac55b726a1
Cookie <1549029765@qq.com>
2024-03-07 09:40:31 +0800

    生成器函数

---
+++ ThirdLearn/yeild_l.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) Cookie Yang. All right reserved.
+"""
+def foo():
+    print("starting...")
+    while True:
+        res = yield 4
+        print("res:",res)
+g = foo()
+print(next(g))
+print("*"*20)
+print(next(g))
